b458e7bd9fa9a3fa88bb5e31c714bfd35cb3fc1d
Wurzeltroll <freytag_andreas@web.de>
2015-09-01 01:56:28 +0200

    Initial commit.

---
+++ crc32.py
@@ -0,0 +1,19 @@
+import hexchat
+import binascii
+from thread import start_new_thread
+
+__module_name__ = "auto-crc32"
+__module_author__ = "Wurzeltroll"
+__module_version__ = "0.1"
+__module_description__ = "Calculate CRC32 after finishing DCC Download"
+
+def calcThread(fname):
+    buf = open(fname,'rb').read()
+    buf = (binascii.crc32(buf) & 0xFFFFFFFF)
+    print("CRC32: %08X" % buf)
+
+def calcCRC32(word, word_eol):
+    start_new_thread(calcThread,(word[1]))
+    return hexchat.EAT_NONE
+
+hexchat.hook_print("DCC RECV Complete",calcCRC32)
