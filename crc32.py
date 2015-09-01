d2984d2c58aa507889b2d760cd337ef539b34d06
Wurzeltroll <freytag_andreas@web.de>
2015-09-01 02:28:53 +0200

    Initial commit was broken... fixed it.

--- crc32.py
+++ crc32.py
@@ -4,7 +4,7 @@ from thread import start_new_thread
 
 __module_name__ = "auto-crc32"
 __module_author__ = "Wurzeltroll"
-__module_version__ = "0.1"
+__module_version__ = "0.2"
 __module_description__ = "Calculate CRC32 after finishing DCC Download"
 
 def calcThread(fname):
@@ -12,8 +12,8 @@ def calcThread(fname):
     buf = (binascii.crc32(buf) & 0xFFFFFFFF)
     print("CRC32: %08X" % buf)
 
-def calcCRC32(word, word_eol):
-    start_new_thread(calcThread,(word[1]))
+def completeHandler(word, word_eol,userdata):
+    start_new_thread(calcThread,(word[1],))
     return hexchat.EAT_NONE
 
-hexchat.hook_print("DCC RECV Complete",calcCRC32)
+hexchat.hook_print("DCC RECV Complete",completeHandler)
