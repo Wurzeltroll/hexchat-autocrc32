64af25fb77f71cd85b07be293d90ec16b9dbe699
Wurzeltroll <freytag_andreas@web.de>
2015-09-01 20:50:46 +0200

    Better output.

--- crc32.py
+++ crc32.py
@@ -4,16 +4,16 @@ from thread import start_new_thread
 
 __module_name__ = "auto-crc32"
 __module_author__ = "Wurzeltroll"
-__module_version__ = "0.2"
+__module_version__ = "0.2.1"
 __module_description__ = "Calculate CRC32 after finishing DCC Download"
 
-def calcThread(fname):
-    buf = open(fname,'rb').read()
+def calcThread(path,fname):
+    buf = open(path,'rb').read()
     buf = (binascii.crc32(buf) & 0xFFFFFFFF)
-    print("CRC32: %08X" % buf)
+    print("CRC32 for {0}: {1:08X}".format(fname, buf))
 
 def completeHandler(word, word_eol,userdata):
-    start_new_thread(calcThread,(word[1],))
+    start_new_thread(calcThread,(word[1],word[0]))
     return hexchat.EAT_NONE
 
 hexchat.hook_print("DCC RECV Complete",completeHandler)
